8da952b49c9583a6c246a51cc9f0dddd5b5ac371
alejandro <alejandrojuarez675@gmail.com>
2019-10-14 15:16:31 -0300

    first commit

---
+++ precache-manifest.adf9addbe9ffcfb6eb09bdd8c6cc4650.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75fca813b1b80f8b939b3b9e7cfcd84a",
+    "url": "/rank-me/index.html"
+  },
+  {
+    "revision": "10937c200c8b64f69e9f",
+    "url": "/rank-me/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "1dd61eb25474f21fc22d",
+    "url": "/rank-me/static/js/2.81fed415.chunk.js"
+  },
+  {
+    "revision": "10937c200c8b64f69e9f",
+    "url": "/rank-me/static/js/main.e7cedf05.chunk.js"
+  },
+  {
+    "revision": "58e32b1d12be49a8f9d0",
+    "url": "/rank-me/static/js/runtime-main.8acb19c6.js"
+  }
+]);
